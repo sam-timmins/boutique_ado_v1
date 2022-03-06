9c643323100f2dff57682926b207d1b4a3f14e79
sam-timmins <sam.timmins1@gmail.com>
2022-03-06 14:45:44 +0000

    Finish product size logic

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
@@ -10,12 +11,29 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # if a product with a size has been added
+    if size:
+        # check if an item with the same id and size is already in the bag, if so increment for that size
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                # if the item is in the bag, but thias is a new size add to the bag
+                bag[item_id]['items_by_size'][size] = quantity
+        # if the item is not already in the bag, add the item
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # if the item has no size
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
