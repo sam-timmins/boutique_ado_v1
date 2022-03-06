c8e9ef3721771540b2c95061da0b9010ff257eeb
sam-timmins <sam.timmins1@gmail.com>
2022-03-06 11:25:35 +0000

    Add bag context and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -160,3 +161,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
